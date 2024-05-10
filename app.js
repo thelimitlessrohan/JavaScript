c59bfa9b1539432e83ce2dbe575eb672c75de01e
Rohan S <rohansable4444@gmail.com>
2024-05-11 02:05:35 +0530

    first

---
+++ app.js
@@ -0,0 +1,68 @@
+//practice 1
+// let size = 'L';
+
+// if (size==='XL'){
+//     console.log('Price is 250');
+// }else if (size==='L'){
+//     console.log('Price is 200');
+// }else if (size==='M'){
+//     console.log('Price is 100');
+// }else if (size==='S'){
+//     console.log('Price is 50');
+// }
+
+
+// Practice 2
+// let goodString = 'apple';
+
+// if (goodString[0]==='a' && goodString.length > 3){
+//     console.log(`${goodString} is a good string `);
+// }
+// else{
+//     console.log(`${goodString} is not a good string `);
+// }
+
+
+// Practice 3
+
+// let size = 'o';
+
+// switch(size){
+//     case "XL":
+//         console.log('Price is 250');
+//         break;
+//     case "L":
+//         console.log('Price is 200');
+//         break;
+//     case "M":
+//         console.log('Price is 100');
+//         break;
+//     case "M":
+//         console.log('Price is 50');
+//         break;
+//     default :
+//     console.log('Size undefined');
+// }
+
+//alert('Something is wrong!');
+//console.error("this is error msg");
+
+
+// let firstName = prompt("Enter Your Name :");
+// alert(`Your name is ${firstName}`);
+
+
+// Strings
+
+// let msg = 'help!';
+// let newmsg = msg.trim().toUpperCase();
+// console.log(newmsg);
+// let news = msg.toUpperCase().trim();
+// console.log(news.indexOf("R"));
+
+//multiplication
+
+// let n = 5
+// for (let i=n; i<=10*n; i= i+n){
+//     console.log(i);
+// }
